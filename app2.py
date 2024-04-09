f27ece57c424592e2121fc89e8c95bafd7a3ebeb
NicolasRP30 <166547526+NicolasRP30@users.noreply.github.com>
2024-04-09 14:42:24 -0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -31,3 +31,38 @@ for feature in parsed_data['features']:
     st.write("Barrio:", barrio)
     st.write("Dirección:", direccion)
     st.write()
+
+nm= st.slider('Selecciona el número de registros de accidentes quieres visualizar', 5, 1500)
+#Construir la tabla de datos (dataframe)
+dfLa = pd.DataFrame({'lat':La[0 : nm]})
+dfLo = pd.DataFrame({'lon':Lo[0 : nm]})
+dfdia= pd.DataFrame({'día' :day[0:nm]})
+dfhor= pd.DataFrame({'Hora' :hour[0:nm]})
+dfbarr=pd.DataFrame({'Barrio':neig[0:nm]})
+dfdir=pd.DataFrame({'Dirección':dir[0:nm]})
+df_g=pd.concat([dfLa, dfLo, dfdia, dfhor,dfdir,dfbarr], axis=1)
+
+# Mostrar la tabla de datos (dataframe)
+st.dataframe(df_g)
+#Dibujar el mapa utilizando las columnas 'lat', 'lon'.
+st.map(df_g)
+
+# Realizar un filtrado de los datos
+st.subheader('Filtrado')
+option_hour_min = st.selectbox('Selecciona filtro por Hora',
+                               ('08:00:00', '09:00:00', '10:00:00','11:00:00','12:00:00','13:00:00','14:00:00'),key='1')
+#option_hour_max = st.selectbox('Selecciona filtro por Hora',
+#                               ('08:00:00', '09:00:00', '10:00:00','11:00:00','12:00:00','13:00:00','14:00:00'),key='2')
+option_day = st.selectbox('Selecciona filtro por día',('LUNES', 'MARTES', 'MIÉRCOLES','JUEVES','VIERNES','SÁBADO','DOMINGO'))
+#df_filtrado = df_g.query('día == "MIÉRCOLES" and Hora >= "08:00:00" and Hora <= "10:00:00"')
+df_filtrado = df_g.query('día == @option_day and Hora >=  @option_hour_min ')
+st.dataframe(df_filtrado)
+
+try:
+   st.metric("Cantidad de Incidentes dentro del filtro", df_filtrado.shape[0])
+except:
+    pass
+
+
+st.map(df_filtrado)
+#st.write(df)
